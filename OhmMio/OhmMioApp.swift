59d9a15f8b7760f91233469cb79542a0e1324870
Emiliano Ruiz Plancarte <emiliano.rplancarte@hotmail.com>
2026-05-05 02:45:49 -0600

    Initial Commit

---
+++ OhmMio/OhmMioApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhmMioApp.swift
+//  OhmMio
+//
+//  Created by Emiliano Ruíz Plancarte on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct OhmMioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OhmMioTests/OhmMioTests.swift
@@ -0,0 +1,19 @@
+//
+//  OhmMioTests.swift
+//  OhmMioTests
+//
+//  Created by Emiliano Ruíz Plancarte on 05/05/26.
+//
+
+import Testing
+@testable import OhmMio
+
+struct OhmMioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
